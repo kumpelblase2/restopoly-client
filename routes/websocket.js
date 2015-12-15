214c410eb07bc1f7564de55c283d8cc6bb44cb05
Tim Hagemann <tim@infinityblade.de>
2015-12-15 22:49:55 +0100

    Readd server and move client into public.

---
+++ routes/websocket.js
@@ -0,0 +1,40 @@
+var router = require('express').Router();
+
+module.exports = function(app) {
+    router.post('/:player/player/turn', function(req, res) {
+        var playerId = req.params.id;
+        var player = app.PlayerStore.getPlayer(playerId);
+        if(player) {
+            player.socket.send('turn');
+            res.status(200).end();
+        } else {
+            res.status(404).end();
+        }
+    });
+    router.post('/:player/player/event', function(req, res) {
+        var playerId = req.params.id;
+        var player = app.PlayerStore.getPlayer(playerId);
+        if(player) {
+            player.socket.send('event', req.body);
+            res.status(200).end();
+        } else {
+            res.status(404).end();
+        }
+    });
+    router.get('/:player/player', function() {
+        var playerId = req.params.id;
+        var player = app.PlayerStore.getPlayer(playerId);
+        if(player) {
+            res.send({
+                id: player.id,
+                name: player.name,
+                ready: player.ready,
+                uri: player.uri
+            });
+        } else {
+            res.status(404).end();
+        }
+    });
+
+    return router;
+};
